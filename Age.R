57e2e10d23fe22cb861cd08e7ee9c35ec9dc4530
krithik45 <113233327+krithik45@users.noreply.github.com>
2023-01-11 09:39:05 +0530

    Create Age.R

---
+++ Age.R
@@ -0,0 +1,12 @@
+{
+  age <- as.integer(readline(prompt = "Enter your age :"))
+  
+  if (age >= 18) {
+    print(paste("You are valid for voting :", age))
+  } else{
+    print(paste("You are not valid for voting :", age))
+  }
+  
+}
+Footer
+© 2023 GitHub, Inc.
